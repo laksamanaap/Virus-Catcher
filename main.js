9f430a1e6faa56c2555ef70e1d7cbf93d59a462e
laksamanaap <laksamana.arya1412@gmail.com>
2023-12-10 23:11:35 +0700

    feat : try draw triangle using html canvas

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-alert("laksa");
+function draw() {
+  const canvas = document.getElementById("game-pages");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.moveTo(45, 30);
+    ctx.lineTo(30, 60);
+    ctx.lineTo(60, 60);
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+
+draw();
