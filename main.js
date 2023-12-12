2778a2e350f90d8b0828bf29087f3a1d812d704b
laksamanaap <laksamana.arya1412@gmail.com>
2023-12-12 20:31:01 +0700

    feat : preparing for virus catcher

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-function draw() {
-  const canvas = document.getElementById("game-pages");
-  if (canvas.getContext) {
-    const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("game-pages");
+const ctx = canvas.getContext("2d");
 
+const ctxWidth = window.innerWidth;
+const ctxHeight = window.innerHeight;
+
+canvas.width = ctxWidth;
+canvas.height = ctxHeight;
+
+canvas.style.background = "aquamarine";
+
+class Circle {
+  constructor(x, y, radius, color, strokeColor) {
+    this.x = x; // "this" is refer to function according to the function itself
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.strokeColor = strokeColor;
+  }
+
+  draw() {
     ctx.beginPath();
-    ctx.moveTo(45, 30);
-    ctx.lineTo(30, 60);
-    ctx.lineTo(60, 60);
-    ctx.closePath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.strokeStyle = this.strokeColor; // Strokex Style
     ctx.stroke();
+    ctx.closePath();
   }
 }
 
-draw();
+// x, y, radius, color, strokeColor
+let circle = new Circle(100, 100, 30, "red", "red"); // Create new instance / object of Circle
+circle.draw(); // Aplying draw function in circle class
