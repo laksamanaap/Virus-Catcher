32a0c5cff5de4dcb87a11d2c80028fad148e65a1
laksamanaap <laksamana.arya1412@gmail.com>
2023-12-12 21:00:06 +0700

    feat : preparing for virus catcher

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const canvas = document.getElementById("game-pages");
 const ctx = canvas.getContext("2d");
 
-const ctxWidth = window.innerWidth;
-const ctxHeight = window.innerHeight;
+const ctx_width = window.innerWidth;
+const ctx_height = window.innerHeight;
 
-canvas.width = ctxWidth;
-canvas.height = ctxHeight;
+canvas.width = ctx_width;
+canvas.height = ctx_height;
 
 canvas.style.background = "aquamarine";
 
@@ -18,16 +18,33 @@ class Circle {
     this.strokeColor = strokeColor;
   }
 
-  draw() {
+  draw(ctx) {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     ctx.fillStyle = this.color;
-    ctx.strokeStyle = this.strokeColor; // Strokex Style
+    ctx.strokeStyle = this.strokeColor; // Stroke style color
+    ctx.lineWidth = 3;
     ctx.stroke();
     ctx.closePath();
   }
 }
 
+let all_circles = [];
+
+function createCircle(circle) {
+  circle.draw(ctx);
+}
+
+// Create Random Circle
+for (let num = 0; num < 10; num++) {
+  const random_x = Math.random() * window.innerWidth;
+  const random_y = Math.random() * window.innerHeight;
+
+  let circle = new Circle(random_x, random_y, 30, "red", "red");
+  all_circles.push(circle);
+  createCircle(all_circles[num]);
+}
+
 // x, y, radius, color, strokeColor
 let circle = new Circle(100, 100, 30, "red", "red"); // Create new instance / object of Circle
-circle.draw(); // Aplying draw function in circle class
+circle.draw(ctx); // Aplying draw function in circle
